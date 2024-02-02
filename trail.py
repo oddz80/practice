a856296cd0ba7832ea3a26ba0c83e15283fa48a1
oddz80 <oddfredriklaastad@protonmail.com>
2024-02-02 14:49:13 +0100

    i did some conversion

--- trail.py
+++ trail.py
@@ -1,7 +1,12 @@
 def main():
     print('python prcatice')
 
+def to_decimal_degrees(deg, min, sec):
+    result = deg/60 + min/(60) + sec/(60*60)
+    return result
 
+a = to_decimal_degrees(59,10,45)
+print(a)
 
 
 if __name__ == "__main__":
